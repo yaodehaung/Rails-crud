bbd3d924ecc2b92a4346225b001156cca047c9cb
yaodehaung <yaodehaung@yaodehaungde-MacBook-Air.local>
2017-11-02 00:32:23 +0800

    first commit

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,28 @@
+class CandidatesController < ApplicationController
+
+
+
+  def index
+    @candidates = Candidate.all
+    end
+
+  def new
+     @candidate = Candidate.new
+   end
+
+  def create
+      @candidate = Candidate.new(candidate_params)
+
+      if @candidate.save
+        # 成功
+        redirect_to candidates_path, notice: "新增候選人成功!"
+      else
+        # 失敗
+        render :new
+      end
+    end
+    private
+        def candidate_params
+          params.require(:candidate).permit(:name, :age, :party, :politics)
+        end
+end
